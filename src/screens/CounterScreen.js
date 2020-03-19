842a2e83e38d2a8870cb14bf3c85896618be7934
bartukaan <bartukaanpideci@gmail.com>
2020-03-19 11:22:35 +0300

    Conter screen using reduce

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,25 +1,31 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 
+//action = howToChangeState
+const reducer = (state, action) => {
+  //state === {counter : number}
+  //action === {type:'increment' || ''decrement , payload: 1}
+
+  switch (action.type) {
+    case "increment":
+      return {...state,count:state.count + action.payload}
+    case "decrement":
+      return {...state,count:state.count - action.payload}
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  //const [counter, setCounter] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
   return (
     <View>
-      <Button
-        title="Increase"
-        onPress={() => {
-          setCounter(counter + 1);
-          console.log(counter);
-        }}
-      />
-      <Button
-        title="Decrease"
-        onPress={() => {
-          setCounter(counter - 1);
-          console.log(counter);
-        }}
-      />
-      <Text>Counter Count: {counter}</Text>
+      <Button title="Increase" onPress={() => {dispatch ({ type:'increment',payload:-1})}} />
+      <Button title="Decrease" onPress={() => {dispatch ({ type:'decrement',payload:-1})}} />
+      <Text>Counter Count: {state.count}</Text>
     </View>
   );
 };
