e322536c13a3e53701e0ef2b9f101c34c0e381bc
bartukaan <bartukaanpideci@gmail.com>
2020-03-19 16:43:21 +0300

    colored box position example 2

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   parentStyle: {
     borderWidth: 3,
     borderColor: "black",
-    height: 100,
+    height: 200,
     flexDirection: "row",
     justifyContent: "space-between"
   },
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     width: 50,
     backgroundColor: "green",
     marginTop: 50,
-    alignSelf:'flex-end'
+    //alignSelf:'flex-end'
   },
   viewThreeStyle: {
     height: 50,
